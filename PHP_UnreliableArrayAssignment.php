5afbd964c3355af69fc5eae537ff266aaf0a61bd
sliceofcake <sliceofcake@users.noreply.github.com>
2016-11-02 22:42:29 -0700

    clarification in comment

--- PHP_UnreliableArrayAssignment.php
+++ PHP_UnreliableArrayAssignment.php
@@ -1,6 +1,6 @@
 // PHP equals-sign assignment with arrays can goof
 // Read this program, guess what happens, and then check the actual output by running it
-// Spoiler : If you ever have a living variable that references part of an array, that part of the array will be ~referenced copied~ when you use equals-sign assignment. Note that as soon as that variable is unset($_), things will return to normal. To see this, try adding the code line "unset($unused);" immediately after the line where $unused is assigned. Now the copies will remain unchanged, as expected. The conclusion is that you shouldn't trust equals-sign assignment to copy an array, you should always use a custom deep-copy function when you need to copy an array.
+// Spoiler : If you ever have a living variable that references part of an array, that part of the array will be ~referenced copied~ when you use equals-sign assignment. Note that if you unset($_) the variable before you make another copy, things will return to normal. To see this, try adding the code line "unset($unused);" immediately after the line where $unused is assigned. Now the copies will remain unchanged, as expected. The conclusion is that you shouldn't trust equals-sign assignment to copy an array, you should always use a custom deep-copy function when you need to copy an array.
 
 // make an array with two key-value pairs
 $v = ["a"=>1,"b"=>2];
